3dbde8bf7a41c53076e5729e9854aed743bac85f
Anshul <anshulsoni2003@gmail.com>
2023-06-12 14:24:01 +0530

    deleted material ui core

--- src/components/Predict/Predict.jsx
+++ src/components/Predict/Predict.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { Card, Typography, Button } from '@material-ui/core';
 
 function Predict(props) {
   const { jobs } = props;
 
   return (
     <div>
-      <Typography className="text-gradient" variant="h2" align="center">
+      <h2 className="text-gradient text-center text-4xl font-bold mb-10">
         Your Recommended Job
-      </Typography>
+      </h2>
 
-      <Card style={{ margin: '30px', padding: '20px' }}>
-        <Typography variant="h4" gutterBottom>
+      <div className="bg-white rounded-md shadow-md p-6 m-6">
+        <h4 className="text-2xl font-bold mb-4">
           {jobs.map((job, index) => (
             <div key={index}>
               <span>{job}</span>
               <br />
             </div>
           ))}
-        </Typography>
-      </Card>
+        </h4>
+      </div>
 
-      <Button  variant="contained" color="primary" onClick={() => window.location.reload()}>
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-6"
+        onClick={() => window.location.reload()}
+      >
         Go AGAIN
-      </Button>
+      </button>
     </div>
   );
 }
